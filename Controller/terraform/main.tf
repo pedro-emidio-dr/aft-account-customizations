9701ce9846450770b53a7748952db2d4127abcbc
pedro-emidio-dr <pedro.emidio@datarain.com.br>
2023-11-03 16:46:09 -0300

    teste

--- Controller/terraform/main.tf
+++ Controller/terraform/main.tf
@@ -1,16 +1,24 @@
 data "aws_caller_identity" "current" {}
 
-locals {
-  analyzer_name = "main_analyzer"
-}
+# locals {
+#   analyzer_name = "main_analyzer"
+# }
 
-resource "aws_organizations_organization" "main_aws_services" {
-  aws_service_access_principals = ["access-analyzer.amazonaws.com"]
-}
+# resource "aws_organizations_organization" "main_aws_services" {
+#   aws_service_access_principals = ["access-analyzer.amazonaws.com"]
+# }
 
-resource "aws_accessanalyzer_analyzer" "main_analyzer" {
-  analyzer_name = local.analyzer_name
-  type          = "ACCOUNT"
+# resource "aws_accessanalyzer_analyzer" "main_analyzer" {
+#   analyzer_name = local.analyzer_name
+#   type          = "ACCOUNT"
 
-  depends_on = [aws_organizations_organization.example]
+#   depends_on = [aws_organizations_organization.example]
+# }
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket-${data.aws_caller_identity.current.account_id}"
+
+  tags = {
+    Environment = "Dev"
+  }
 }
