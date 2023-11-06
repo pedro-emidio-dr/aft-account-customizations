92977a2fc3066c83826bb1bcaa3efe499c502658
pedro-emidio-dr <pedro.emidio@datarain.com.br>
2023-11-06 10:57:23 -0300

    access analyzer

--- management/terraform/main.tf
+++ management/terraform/main.tf
@@ -2,13 +2,11 @@ locals {
   analyzer_name = "main_analyzer"
 }
 
-resource "aws_organizations_organization" "main_aws_services" {
-  aws_service_access_principals = ["access-analyzer.amazonaws.com"]
-}
+# resource "aws_organizations_organization" "main_aws_services" {
+#   aws_service_access_principals = ["access-analyzer.amazonaws.com"]
+# }
 
 resource "aws_accessanalyzer_analyzer" "main_analyzer" {
   analyzer_name = local.analyzer_name
   type          = "ACCOUNT"
-
-  depends_on = [aws_organizations_organization.main_aws_services]
 }
