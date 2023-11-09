8a4bb0f0233b353d73e6c4112f6a5aa6253226f6
pedro-emidio-dr <pedro.emidio@datarain.com.br>
2023-11-09 17:58:11 -0300

    create event bus

--- workloads/terraform/event_bus_hub.tf
+++ workloads/terraform/event_bus_hub.tf
@@ -1,8 +1,5 @@
-# module "hub"{
-#     source = "./modules/event_bus_hub"
+module "hub"{
+    source = "./modules/event_bus_hub"
 
-#     pOrganizationId = "o-5pdcv5dwqk"
-# }
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket-${data.aws_caller_identity.current.account_id}"
+    pOrganizationId = "o-5pdcv5dwqk"
 }
--- workloads/terraform/modules/event_bus_hub/provider.tf
+++ workloads/terraform/modules/event_bus_hub/provider.tf
@@ -10,8 +10,3 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-
-
-data "aws_region" "current" {}
-
-data "aws_caller_identity" "current" {}
