6bd19ee87a09e7b456946f0a4f3cbd2ab9fa926c
pedro-emidio-dr <pedro.emidio@datarain.com.br>
2023-11-21 15:29:37 -0300

    update

--- workloads/terraform/modules/event_bus/main.tf
+++ workloads/terraform/modules/event_bus/main.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_role" "cross_account_role" {
   count = length(var.source_event_account_id)
-  name = "CrossAccountEventBusRole"
+  name = "CrossAccountEventBusRole_${var.source_event_account_id[count.index]}"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -25,7 +25,7 @@ EOF
 resource "aws_iam_role_policy_attachment" "attach_policy" {
   count = length(var.source_event_account_id)
   policy_arn = "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess"
-  role       = aws_iam_role.cross_account_role.name
+  role       = aws_iam_role.cross_account_role[count.index].name
 }
 
 
