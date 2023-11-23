5c388f5925190c4772298dce0f568a7367a59ec9
pedro-emidio-dr <pedro.emidio@datarain.com.br>
2023-11-23 16:11:13 -0300

    teste

--- workloads/terraform/modules/hub_account/main.tf
+++ workloads/terraform/modules/hub_account/main.tf
@@ -9,52 +9,10 @@ resource "aws_cloudwatch_event_permission" "example" {
   event_bus_name = aws_cloudwatch_event_bus.main_event_bus.name
 }
 
-resource "aws_iam_role" "event_bus_invoke_remote_event_bus" {
-  name               = "InvokeRemoteEventBus"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = {
-        Service = "events.amazonaws.com"
-      },
-      Action = "sts:AssumeRole",
-    }],
-  })
-}
-
-resource "aws_iam_policy" "event_bus_invoke_remote_event_bus" {
-  name   = "invoke_remote_event_bus"
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect    = "Allow",
-        Action    = "events:PutEvents",
-        Resource  = aws_cloudwatch_event_bus.main_event_bus.arn
-      },
-      {
-        Effect    = "Allow",
-        Action    = "sns:Publish",
-        Resource  = var.arn_of_target
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "event_bus_invoke_remote_event_bus_custom" {
-  role       = aws_iam_role.event_bus_invoke_remote_event_bus.name
-  policy_arn = aws_iam_policy.event_bus_invoke_remote_event_bus.arn
-}
-
-resource "aws_iam_role_policy_attachment" "event_bus_invoke_remote_event_bus" {
-  role       = aws_iam_role.event_bus_invoke_remote_event_bus.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess"
-}
-
 resource "aws_cloudwatch_event_rule" "default_event_rule" {
   name           = var.rule_name
   description    = var.descripiton_rule
+  event_bus_name = aws_cloudwatch_event_bus.main_event_bus.arn
   event_pattern  = <<PATTERN
 {
   "source": ["aws.events"]
@@ -66,5 +24,4 @@ resource "aws_cloudwatch_event_target" "default_event_target" {
   target_id = "eventBusEvents"
   arn       = var.arn_of_target
   rule      = aws_cloudwatch_event_rule.default_event_rule.name
-  # role_arn  = aws_iam_role.event_bus_invoke_remote_event_bus.arn
 }
