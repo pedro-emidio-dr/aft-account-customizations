ba4d03ecceeb92355e2c1bf6717aef2a9884b8fa
pedro-emidio-dr <pedro.emidio@datarain.com.br>
2023-11-23 10:18:35 -0300

    teste

--- workloads/terraform/modules/event_bus/main.tf
+++ workloads/terraform/modules/event_bus/main.tf
@@ -3,8 +3,9 @@ resource "aws_cloudwatch_event_bus" "main_event_bus" {
 }
 
 resource "aws_cloudwatch_event_permission" "example" {
-  count = length(var.source_event_account_id)
-  statement_id  = "Cross_account_permission_${var.source_event_account_id[count.index]}"
-  principal     = var.source_event_account_id[count.index]
+  count          = length(var.source_event_account_id)
+  statement_id   = "Cross_account_permission_${var.source_event_account_id[count.index]}"
+  principal      = var.source_event_account_id[count.index]
   event_bus_name = aws_cloudwatch_event_bus.main_event_bus.name
 }
+
