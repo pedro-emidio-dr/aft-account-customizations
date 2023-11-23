268d3a02f20ce62b5ea9a7b6cb5fdc10cb08b4b7
pedro-emidio-dr <pedro.emidio@datarain.com.br>
2023-11-23 17:32:08 -0300

    teste

--- workloads/terraform/modules/hub_account/main.tf
+++ workloads/terraform/modules/hub_account/main.tf
@@ -2,11 +2,16 @@ resource "aws_cloudwatch_event_bus" "main_event_bus" {
   name = var.event_bus_name
 }
 
-resource "aws_cloudwatch_event_permission" "example" {
+resource "aws_cloudwatch_event_permission" "OrganizationAccess" {
   count          = length(var.source_event_account_id)
   statement_id   = "Cross_account_permission_${var.source_event_account_id[count.index]}"
-  principal      = var.source_event_account_id[count.index]
+  principal      = "*"
   event_bus_name = aws_cloudwatch_event_bus.main_event_bus.name
+  condition {
+    key   = "aws:PrincipalOrgID"
+    type  = "StringEquals"
+    value = data.aws_organizations_organization.current.id
+  }
 }
 
 resource "aws_cloudwatch_event_rule" "default_event_rule" {
