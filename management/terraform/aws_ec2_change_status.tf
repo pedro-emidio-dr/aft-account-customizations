a8f5597d344962903c5d7db53efabca364b90efe
pedro-emidio-dr <pedro.emidio@datarain.com.br>
2023-12-12 15:54:46 -0300

    teste

--- management/terraform/aws_ec2_change_status.tf
+++ management/terraform/aws_ec2_change_status.tf
@@ -1,10 +1,6 @@
 module "spoker-us-east-1" {
   source = "./modules/spoke_account"
 
-  providers  = {
-    aws = aws.us_east_1
-  }
-
   rule_name = "getEC2ChangeStatus"
   descripiton_rule = ""
   event_pattern_rule = <<PATTERN
@@ -22,30 +18,4 @@ PATTERN
 
 event_bus_name = "arn:aws:events:us-east-1:065058211633:event-bus/EventBusCriticalAlerts"
 target_id = "getEC2ChangeStatus"
-}
-
-module "spoker-sp" {
-  source = "./modules/spoke_account"
-
-  providers  = {
-    aws = aws.sa_east_1
-  }
-
-  rule_name = "getEC2ChangeStatus"
-  descripiton_rule = ""
-  event_pattern_rule = <<PATTERN
-{
-  "source": ["aws.ec2"],
-  "detail-type": ["AWS API Call via CloudTrail"],
-  "detail": {
-    "eventSource": ["ec2.amazonaws.com"],
-    "eventName": ["StopInstances","RunInstances","StartInstances","TerminateInstances"]
-    "requestParameters.tagSpecificationSet.items.tags.value": [{ "anything-but": "True"}],
-    "requestParameters.tagSpecificationSet.items.tags.key": ["IsEksCluster"],
-  }
-}
-PATTERN
-
-  event_bus_name = "arn:aws:events:us-east-1:065058211633:event-bus/EventBusCriticalAlerts"
-  target_id = "getEC2ChangeStatus"
 }
