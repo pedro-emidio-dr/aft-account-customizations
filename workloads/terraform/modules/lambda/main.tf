048320a1fd7a21d1a76139a0053f8a4c31633878
pedro-emidio-dr <pedro.emidio@datarain.com.br>
2023-12-15 16:17:29 -0300

    add lambda

--- workloads/terraform/modules/lambda/main.tf
+++ workloads/terraform/modules/lambda/main.tf
@@ -65,7 +65,7 @@ data "archive_file" "source" {
 resource "aws_lambda_function" "main_lambda" {
   function_name    = "filter-ec2-tags"
   filename         = "lambda_function.zip"
-  source_code_hash = data.archive_file.lambda_code.output_base64sha256
+  source_code_hash = data.archive_file.source.output_base64sha256
   handler          = "lambda_function.lambda_handler"
   role             = aws_iam_role.main_role.arn
   runtime          = "python3.12"
