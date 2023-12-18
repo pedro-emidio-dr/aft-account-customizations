ba07d7adde12af2d731c0e7f270cbe5c41a2c63a
pedro-emidio-dr <pedro.emidio@datarain.com.br>
2023-12-18 10:17:42 -0300

    teste de lambda

--- management/terraform/modules/spoke_rule_with_lambda/main.tf
+++ management/terraform/modules/spoke_rule_with_lambda/main.tf
@@ -1,38 +1,38 @@
-resource "random_integer" "random_number" {
-  min = 1
-  max = 5000
-}
+# resource "random_integer" "random_number" {
+#   min = 1
+#   max = 5000
+# }
 
-resource "aws_iam_role" "event_bus_invoke_remote_event_bus" {
-  name               = "InvokeRemoteEventBus_${var.rule_name}_${random_integer.random_number.result}"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = {
-        Service = "events.amazonaws.com"
-      },
-      Action = "sts:AssumeRole",
-    }],
-  })
-}
+# resource "aws_iam_role" "event_bus_invoke_remote_event_bus" {
+#   name               = "InvokeRemoteEventBus_${var.rule_name}_${random_integer.random_number.result}"
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [{
+#       Effect = "Allow",
+#       Principal = {
+#         Service = "events.amazonaws.com"
+#       },
+#       Action = "sts:AssumeRole",
+#     }],
+#   })
+# }
 
-resource "aws_iam_policy" "event_bus_invoke_remote_event_bus" {
-  name   = "invoke_remote_event_bus_${var.rule_name}_${random_integer.random_number.result}"
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect    = "Allow",
-      Action: "lambda:InvokeFunction",
-      Resource  = var.target_arn
-    }]
-  })
-}
+# resource "aws_iam_policy" "event_bus_invoke_remote_event_bus" {
+#   name   = "invoke_remote_event_bus_${var.rule_name}_${random_integer.random_number.result}"
+#   policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [{
+#       Effect    = "Allow",
+#       Action: "lambda:InvokeFunction",
+#       Resource  = var.target_arn
+#     }]
+#   })
+# }
 
-resource "aws_iam_role_policy_attachment" "event_bus_invoke_remote_event_bus_custom" {
-  role       = aws_iam_role.event_bus_invoke_remote_event_bus.name
-  policy_arn = aws_iam_policy.event_bus_invoke_remote_event_bus.arn
-}
+# resource "aws_iam_role_policy_attachment" "event_bus_invoke_remote_event_bus_custom" {
+#   role       = aws_iam_role.event_bus_invoke_remote_event_bus.name
+#   policy_arn = aws_iam_policy.event_bus_invoke_remote_event_bus.arn
+# }
 
 resource "aws_cloudwatch_event_rule" "default_event_rule" {
   name           = var.rule_name
@@ -44,5 +44,5 @@ resource "aws_cloudwatch_event_target" "default_event_target" {
   target_id = var.target_id
   arn       = var.target_arn
   rule      = aws_cloudwatch_event_rule.default_event_rule.name
-  role_arn  = aws_iam_role.event_bus_invoke_remote_event_bus.arn
+  # role_arn  = aws_iam_role.event_bus_invoke_remote_event_bus.arn
 }
